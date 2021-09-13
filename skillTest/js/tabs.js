593126e9c229377649d21d253f69a3578ab02283
AlexRyabov <ryabovsasha88@gmail.com>
2021-09-13 07:25:59 +0300

    added search-bar and burger

--- skillTest/js/tabs.js
+++ skillTest/js/tabs.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.tabs__btn').forEach(function(tabsBtn) {
         tabsBtn.addEventListener('click', function(event) {
             const path = event.currentTarget.dataset.path
-            console.log(path)
+        
             document.querySelectorAll('.tab-content').forEach(function(tabContent) {
                 tabContent.classList.remove('show')
             })
